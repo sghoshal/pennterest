5b2e51c42db9244136d1ec86c8194a71b0f2c4f3
Saghm Rossi <saghmrossi@gmail.com>
2013-12-01 01:53:21 -0500

    removed sql files

--- followers.sql
+++
@@ -1,5 +0,0 @@
-SELECT firstname, lastname FROM Users WHERE userid in (
-    SELECT DISTINCT userid FROM Following WHERE boardid IN (
-        SELECT boardid FROM Board WHERE userid=101
-    )
-);
--- info.sql
+++
@@ -1,8 +0,0 @@
-SELECT U.firstname, U.lastname, U.profile_pic, U.cover_pic, COUNT(B.boardid), COUNT(photoid)
-FROM ((Users U INNER JOIN Board B ON U.userid=B.userid) INNER JOIN Pin P ON  B.boardid=P.boardid) WHERE U.userid IN (SELECT DISTINCT F.userid
-    FROM Following F
-    WHERE F.boardid IN (SELECT B1.boardid
-         FROM BOARD B1
-         WHERE B.userid=101))
-GROUP BY (U.firstname, U.lastname, U.profile_pic, U.cover_pic, U.userid);
-
