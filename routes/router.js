ddae47aa4702989b1dbb11030d67fb2468734089
Saghm Rossi <saghmrossi@gmail.com>
2013-12-07 02:46:40 -0500

    added interest search and following page

--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,10 @@
 var express = require('express'),
-    followers = require('./followers'),
-    search = require('./search');
+    follow = require('./follow/follow'),
+    search = require('./search/search');
 
 exports.app = express()
-exports.app.get('/:id/followers', followers.get);
-exports.app.get('/search', search.get);
+exports.app.get('/:id/followers', follow.getFollowers);
+exports.app.get('/:id/following', follow.getFollowing);
+exports.app.get('/search/users', search.getUsers);
+exports.app.get('/search/photos', search.getPhotos);
+exports.app.get('/search/interests', search.getInterests);
